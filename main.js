39c0b9e20113e76f8dea145662a176a836801e7a
Christine Maynard <christine.ri.maynard@gmail.com>
2021-12-26 13:54:29 -0500

    Add dark theme styling TODO Add JS for light/dark transitioning

---
+++ main.js
@@ -0,0 +1,9 @@
+let toggleBtn = document.querySelector(".toggle-btn");
+let wrapper = document.querySelector(".big-wrapper");
+const main = document.querySelector(".main");
+
+function toggleUI() {
+  // let's create a copy of the homepage
+  let cloneUI = wrapper.cloneNode();
+}
+toggleBtn.addEventListener("click", toggleUI);

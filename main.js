541cd1a97c7d50480049d2d05d97dfad265fa70c
Christine Maynard <christine.ri.maynard@gmail.com>
2021-12-27 11:24:15 -0500

    Add toggle feature to switch betwen light/dark theme

--- main.js
+++ main.js
@@ -1,9 +1,55 @@
-let toggleBtn = document.querySelector(".toggle-btn");
-let wrapper = document.querySelector(".big-wrapper");
-const main = document.querySelector(".main");
+let toggleBtn;
+let wrapper;
+
+/**
+ * To create change between light/dark themes
+ * clone UI
+ * toggle between different UIs by removing/adding corresponding class with associated styles
+ *
+ */
+
+function substituteBtns() {
+  toggleBtn = document.querySelector(".toggle-btn");
+  wrapper = document.querySelector(".wrapper");
+}
+const main = document.querySelector("main");
+substituteBtns();
+
+// user gets light theme by default
+let darkTheme = false;
 
 function toggleUI() {
-  // let's create a copy of the homepage
-  let cloneUI = wrapper.cloneNode();
+  darkTheme = !darkTheme;
+  console.log(darkTheme);
+  // let's create a copy of the homepage as well as descendants of our elements
+  // the copied UI will be layered over the previous UI depending on user preference
+  let cloneUI = wrapper.cloneNode(true);
+  if (darkTheme) {
+    cloneUI.classList.remove("light");
+    cloneUI.classList.add("dark");
+  } else {
+    cloneUI.classList.remove("dark");
+    cloneUI.classList.add("light");
+  }
+  cloneUI.classList.add("copyUi");
+  main.appendChild(cloneUI);
+  // let's remove the scroll feature when switching themes
+  // see CSS rules for removing scroll
+  document.body.classList.add("stop-scrolling");
+
+  cloneUI.addEventListener("animationend", () => {
+    // let's add back the scroll feature when done switching themes
+    document.body.classList.remove("stop-scrolling");
+    wrapper.remove();
+    cloneUI.classList.remove("copyUi");
+    // reset variables
+    substituteBtns();
+    events();
+  });
 }
-toggleBtn.addEventListener("click", toggleUI);
+
+function events() {
+  toggleBtn.addEventListener("click", toggleUI);
+}
+
+events();
